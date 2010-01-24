3e8139177feda862c5eed1dcf18f9561ee441062
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-24 19:37:27 +0100

    au to loa der

--- lib/monkey/autoloader.rb
+++ lib/monkey/autoloader.rb
@@ -1,5 +1,23 @@
 require "monkey"
 
+Module.class_eval do
+  alias const_missing_without_detection const_missing
+
+  def const_missing(const_name)
+    if parent.autoloader? and not is_a? Monkey::Autoloader
+      extend Monkey::Autoloader
+      const_missing const_name
+    else
+      const_missing_without_detection const_name
+    end
+  end
+
+  def autoloader?
+    is_a? Monkey::Autoloader or (parent != self and parent.autoloader?)
+  end
+
+end
+
 module Monkey
   module Autoloader
     def const_missing(const_name)
